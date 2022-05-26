916bdeded0aed07cd352b7ee29e43edd3cef1c9e
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2022-05-26 13:41:53 +0200

    Add canvas + dependencies

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
+import styles from "../styles/Global";
+import { gsap, Power3 } from "gsap";
+
+export default function Header() {
+  let nameItem = useRef(null);
+  let sectorItem = useRef(null);
+  let buttomItem = useRef(null);
+  useEffect(() => {
+    console.log(nameItem);
+    // nameItem.style.display = "none";
+    gsap.to(nameItem, {
+      opacity: 1,
+      duration: 1,
+      y: 0,
+      ease: Power3,
+    });
+    gsap.to(sectorItem, {
+      opacity: 1,
+      duration: 1,
+      delay: 0.5,
+      y: 0,
+      ease: Power3,
+    });
+    gsap.to(buttomItem, {
+      opacity: 1,
+      duration: 1,
+      delay: 1,
+      y: 0,
+      ease: Power3,
+    });
+  }, []);
+
+  const navigate = useNavigate();
+  const handleMain = (e) => {
+    e.preventDefault();
+    navigate("/main");
+  };
+
+  return (
+    <div className="absolute top-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-20 flex justify-center items-center">
+      <div className=" flex flex-col justify-center items-center">
+        <h1
+          ref={(element) => {
+            nameItem = element;
+          }}
+          className="font-Rubik text-3xl font-normal uppercase text-black translate-y-12 opacity-0"
+        >
+          Javier Palomino
+        </h1>
+        <p
+          ref={(element) => {
+            sectorItem = element;
+          }}
+          className="font-Rubik text-6xl font-bold uppercase text-black mt-12 translate-y-12 opacity-0"
+        >
+          Data Developer
+        </p>
+        <buttom
+          ref={(element) => {
+            buttomItem = element;
+          }}
+          className={`${styles.button}, translate-y-10 opacity-0`}
+          type="button"
+          onClick={handleMain}
+        >
+          Portfolio
+        </buttom>
+      </div>
+    </div>
+  );
+}
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,28 +1,16 @@
 import React from "react";
-import { useNavigate } from "react-router";
-// import assets from "../assets";
-import styles from "../styles/Global";
+import { Canvas } from "@react-three/fiber";
+import Header from "./Header";
 
 export default function Landing() {
-  const navigate = useNavigate();
-  const handleMain = (e) => {
-    e.preventDefault();
-    navigate("/main");
-  };
-
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="w-1/3 flex flex-col justify-center items-center">
-        <h1 className="font-Rubik text-2xl uppercase">Javier Palomino</h1>
-        <p className="font-Rubik text-4xl mt-6 uppercase">Data Developer</p>
-        <buttom
-          className={`${styles.button}`}
-          type="button"
-          onClick={handleMain}
-        >
-          Portfolio
-        </buttom>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Canvas>
+        <mesh>
+          <planeBufferGeometry args={[3, 5]} />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
