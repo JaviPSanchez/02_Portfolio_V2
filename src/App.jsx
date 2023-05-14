ee8032a87d5f8a2413b584e006bdd0cf1e8fa30d
JaviPSanchez <javier.palomino-sanchez@outlook.com>
2023-05-14 14:21:15 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center text-4xl font-bold text-red-400">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
